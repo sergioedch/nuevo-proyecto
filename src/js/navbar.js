e20527e7f56e6b1f4115f8944547996640088f1f
Sergio Eduardo <sergiocasarrubias500@gmail.com>
2026-09-14 12:16:39 -0600

    cambio de navbar trancision

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // --- NUEVO CÓDIGO PARA EFECTO BLUR EN SCROLL ---
+    const navbar = document.querySelector('.navbar');
+
+    if (navbar) {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 50) {
+                navbar.classList.add('navbar-scrolled', 'py-2');
+                navbar.classList.remove('pt-4', 'mb-2'); 
+            } else {
+                navbar.classList.remove('navbar-scrolled', 'py-2');
+                navbar.classList.add('pt-4', 'mb-2');
+            }
+        });
+    }
+    
     let rutaActual = window.location.pathname;
     
     if (rutaActual === '/' || rutaActual === '') {
