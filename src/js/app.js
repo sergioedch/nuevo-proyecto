12ac24f60bdfda73d7770461b1d48c4d988d4edd
Sergio Eduardo <sergiocasarrubias500@gmail.com>
2026-09-15 13:10:26 -0600

    Refactor code structure for improved readability and maintainability

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
 // Importamos los estilos de SASS
 import '../scss/main.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 // Importamos todo el JS de Bootstrap
 import * as bootstrap from 'bootstrap';
 
+AOS.init({
+    once: true, // Para que la animación ocurra solo la primera vez que bajas
+    duration: 800 // Cuánto dura la animación en milisegundos
+});
